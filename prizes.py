c897728acc580ca4b980e3553cbe372813430af1
erikkov <erikkov@ulrik.uio.no>
2025-04-04 12:06:25 +0200

    Add more gifs for prizes

--- prizes.py
+++ prizes.py
@@ -5,7 +5,7 @@ class Prize:
     def __init__(self, name: str, description: str, image: str = None):
         self.name = name
         self.description = description
-        self.image = image or "blank"
+        self.image = f'/static/images/{image}' if image else "blank"
 
     def __str__(self) -> str:
         return f"{self.name}: {self.description}"
@@ -42,19 +42,89 @@ class PrizeManager:
         """Load some default prizes for testing"""
         default_prizes = {
             (2025, 14): Prize(
-                name="YT Protein Shake",
-                description="Bimbini gains",
-                image="blank"
+                name="Kveld med Netflix",
+                description="Sett deg ned og kos deg med en god film eller serie",
+                image="Prize_Cinema.gif"
             ),
             (2025, 15): Prize(
-                name="Massage",
-                description="30 min massage",
-                image="blank"
+                name="Spermageddon",
+                description="Greit vi kan se spermageddon (om den fortsatt går, hvis ikke noe anna). + slush",
+                image="Prize_Cinema.gif"
             ),
             (2025, 16): Prize(
-                name="Movie Night",
-                description="Movie of choice + snacks",
-                image="blank"
+                name="YT protein shake",
+                description="El gains de bimbini",
+                image="Prize.gif"
+            ),
+            (2025, 17): Prize(
+                name="Strikkekveld",
+                description="En kveld hvor du lærer meg å strikke",
+                image="Prize_Knit.gif"
+            ),
+            (2025, 18): Prize(
+                name="Pasta night",
+                description="Vi cookåh pasta, my treat",
+                image="Prize_Pasta.gif"
+            ),
+            (2025, 19): Prize(
+                name="Massasje",
+                description="30 minutters massasje når det passer deg",
+                image="Prize_Massage.gif"
+            ),
+            (2025, 20): Prize(
+                name="YT Protein Shake",
+                description="Må huske på å la deg bli bøs også",
+                image="Prize.gif"
+            ),
+            (2025, 21): Prize(
+                name="Bakekveld",
+                description="Vi baker noe digg, du velger hva",
+                image="Prize_Baking.gif"
+            ),
+            (2025, 22): Prize(
+                name="Spa-kveld",
+                description="Ansiktsmaske, cream soda, søt-film, og snacks",
+                image="Prize_Spa.gif"
+            ),
+            (2025, 23): Prize(
+                name="YT Protein Shake",
+                description="munch munch",
+                image="Prize.gif"
+            ),
+            (2025, 24): Prize(
+                name="Tur på museum",
+                description="Du velgåh hvilket",
+                image="Prize.gif"
+            ),
+            (2025, 25): Prize(
+                name="Digital detox kveld",
+                description="Sett deg ned med en god bok og en kopp te. Vi bruker ikke tlf hele kvelden",
+                image="Prize.gif"
+            ),
+            (2025, 26): Prize(
+                name="Fototur i botanisk hage",
+                description="Vi tar en tur sammen og viser hverandre bildene når vi er ferdige",
+                image="Prize.gif"
+            ),
+            (2025, 27): Prize(
+                name="Hjemmetrening",
+                description="Vi trener sammen hjemme",
+                image="Prize.gif"
+            ),
+            (2025, 28): Prize(
+                name="Matlaging",
+                description="Lag en ny oppskrift sammen",
+                image="Prize.gif"
+            ),
+            (2025, 29): Prize(
+                name="Vin og ost",
+                description="Smaking av forskjellige viner og oster",
+                image="Prize.gif"
+            ),
+            (2025, 30): Prize(
+                name="DIY-prosjekt",
+                description="Start på et kreativt prosjekt hjemme",
+                image="Prize.gif"
             )
         }
 
